4b5758a8912841aa78632599d8833cc1d28bd421
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-06 13:10:19 +0200

    completed 5/7 parts of the exercise

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+# Controller for Task Model
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_show_path(@task.id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create", as: :task_create
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update", as: :task_update
+
+  get "tasks/:id", to: "tasks#show", as: :task_show
+
 end
